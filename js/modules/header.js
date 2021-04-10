9dfad3cfdf7ed96e1f14db9efc0c39f8ceda04ed
Ederson <edersonhenrique2016@gmail.com>
2021-04-10 00:09:30 -0300

    Adicionado animation canção

---
+++ js/modules/header.js
@@ -0,0 +1,8 @@
+export default function initHeader(){
+
+const cancao = window.addEventListener('scroll', function(event){
+
+    
+
+})
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import initHeader from './modules/header.js';
 import initscrollSuave from './modules/scroll-suave.js';
 import initborder from './modules/border-menu.js';
 import initList from './modules/historia.js';
