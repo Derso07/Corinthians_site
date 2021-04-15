2ba4e206b3fcc9b116ec26795d8241c3c508c2dc
Ederson <edersonhenrique2016@gmail.com>
2021-04-15 09:29:34 -0300

    finalizado botão mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,13 +1,19 @@
 export default function initMenu(){
 
     const menuBotao = document.querySelector('[data-menu="botao"');
-    const menuLista = document.querySelector('[data-menu-lista');
+    const menuLista = document.querySelector('[data-menu-lista]');
+    const menu = document.querySelector('[data-sroll]');
 
-    function abrirMenu(event){
-        menuBotao.classList.add('ativar');
-        menuLista.classList.add('ativar');
-    
+    function abrirMenu(e){
+        menuBotao.classList.toggle('ativar');
+        menuLista.classList.toggle('ativar');
     }
-    
+
+    function fecharMenu(){
+        menuLista.classList.remove('ativar');
+    }
+ 
     menuBotao.addEventListener('click', abrirMenu);
+    menuLista.addEventListener('click',fecharMenu)
+
 }
