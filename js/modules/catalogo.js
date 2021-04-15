246824b5f599bc3ca9b1eb6e965001c2c82eb8fe
Ederson <edersonhenrique2016@gmail.com>
2021-04-15 10:09:55 -0300

    Iniciado slide

---
+++ js/modules/catalogo.js
@@ -0,0 +1,8 @@
+export default function initCatalogo(){
+    class slide{
+        constructor(slide, catalogo){
+            this.slide = document.querySelector('.slide');
+            this.catalogo = document.querySelector('.catalogo');
+        }
+    }
+}
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import initScrollInterno from './modules/scroll-interno.js';
 import initscrollSuave from './modules/scroll-suave.js';
 import initborder from './modules/border-menu.js';
 import initList from './modules/historia.js';
+import initCatalogo from './modules/catalogo.js';
 import initMenu from './modules/menu-mobile.js';
 
 initHeader();
@@ -10,5 +11,6 @@ initScrollInterno();
 initscrollSuave();
 initborder();
 initList();
+initCatalogo();
 initMenu();
 
