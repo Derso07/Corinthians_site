1040d7659c7bb9053e2fef0b607392cfb86f9703
Ederson <edersonhenrique2016@gmail.com>
2021-04-16 23:53:48 -0300

    Iniciado slide de fotos

--- js/modules/catalogo.js
+++ js/modules/catalogo.js
@@ -1,8 +1,37 @@
-export default function initCatalogo(){
-    class slide{
+export default class Slide{
+  
         constructor(slide, catalogo){
             this.slide = document.querySelector('.slide');
             this.catalogo = document.querySelector('.catalogo');
         }
-    }
-}
+
+        onStart(event){
+            event.preventDefault();
+            this.catalogo.addEventListener('mousemove', this.onMove);
+        }
+
+        onMove(event){
+        }
+
+        ondEnd(event){
+            this.catalogo.removeEventListener('mousemove', this.onMove);
+        }
+
+        addSlideEvents(){
+            this.catalogo.addEventListener('mousedown', this.onStart);
+            this.catalogo.addEventListener('mouseup', this.ondEnd);
+        }
+
+        bindEvents(){
+            this.onStart = this.onStart.bind(this);
+            this.onMove = this.onMove.bind(this);
+            this.ondEnd = this.ondEnd.bind(this);
+        }
+
+        init(){
+            this.bindEvents();
+            this.addSlideEvents();
+                return this;
+            }
+        }
+    
--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@ import initScrollInterno from './modules/scroll-interno.js';
 import initscrollSuave from './modules/scroll-suave.js';
 import initborder from './modules/border-menu.js';
 import initList from './modules/historia.js';
-import initCatalogo from './modules/catalogo.js';
+import Slide from './modules/catalogo.js';
 import initMenu from './modules/menu-mobile.js';
+
+const slide = new Slide('.slide', '.catalogo');
+slide.init();
+
+console.log(slide);
 
 initHeader();
 initScrollInterno();
 initscrollSuave();
 initborder();
 initList();
-initCatalogo();
 initMenu();
 
